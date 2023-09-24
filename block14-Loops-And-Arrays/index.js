99140f07367f6b4eaa5d93f8007258cd2b28370a
Dylan Gage <dylangage2014@gmail.com>
2023-09-24 14:57:16 -0500

    finished Reverse Array

--- block14-Loops-And-Arrays/index.js
+++ block14-Loops-And-Arrays/index.js
@@ -18,12 +18,12 @@ console.log(odds);
 
 //// Vowel vs Consonant
 // Create "vowels" array to comapre the word too.
-// Create input "word"
-// Create 2 counters, 1 for consonants, 1 for vowels
-// Compare each letter in the "word" string with the "vowels" array
-// For each vowel increase the vowel counter
-// For each consonants increase consonants counter
-// Then print the result to the console
+// Create input "word".
+// Create 2 counters, 1 for consonants, 1 for vowels.
+// Compare each letter in the "word" string with the "vowels" array.
+// For each vowel increase the vowel counter.
+// For each consonants increase consonants counter.
+// Then print the result to the console.
 
 const vowels = ["a", "e", "i", "o", "u"];
 const word = "hello";
@@ -41,3 +41,19 @@ for (let i = 0; i < word.length; i++) {
 console.log(
   `${word} has ${consonantsCounter} consonants and ${vowelsCounter} vowels.`
 );
+
+//// Reverse Array
+// Create input "toBeReversed" array.
+// Create placeholder "reversed" output array.
+// use a for loop to go through array and copy each number from the input array to the begining of the output causing the order to be reversed.
+
+const toBeReversed = [1, 2, 3];
+let reversed = [];
+
+for (let i = 0; i < toBeReversed.length; i++) {
+  reversed.unshift(toBeReversed[i]);
+}
+
+console.log(reversed);
+
+
